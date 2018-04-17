f71f3cae812463a70033bf400a6ccabd81b93f31
Damian <dsuarez1@stevens.edu>
2018-04-17 15:32:58 -0400

    Copy from Create Arduino CC

--- PS3BT_test/PS3BT_test.ino
+++ PS3BT_test/PS3BT_test.ino
@@ -13,15 +13,52 @@ USB Usb;
 BTD Btd(&Usb);
 PS3BT PS3(&Btd, 0x00, 0x15, 0x83, 0x3D, 0x0A, 0x57);
 bool switching;
-const int ledPin =   4;
 const int sendPin =  2;       //connect to MEGA
-const int switcher = 7;
+const int tens =   3;
+const int hundreds = 4;
+            //            (2, 4, 7)
+int statuo; //0: stop     (0, 0, 0)
+            //1: right    (0, 0, 1)
+            //2: left     (0, 1, 0)
+            //3: forward  (0, 1, 1)
+            //4: backward (1, 0, 0)
+
+void sendOut(int stat) {
+  switch(stat) {
+    case 3:   //forward
+      digitalWrite(hundreds, LOW); 
+      digitalWrite(tens,    HIGH); 
+      digitalWrite(sendPin, HIGH);
+      break;
+    case 2:   //left
+      digitalWrite(hundreds, LOW); 
+      digitalWrite(tens,     HIGH); 
+      digitalWrite(sendPin,  LOW);
+      break;
+    case 1:   //right
+      digitalWrite(hundreds, LOW); 
+      digitalWrite(tens,     LOW); 
+      digitalWrite(sendPin,  HIGH);
+      break;
+    case 4:   //backward
+      digitalWrite(hundreds, HIGH); 
+      digitalWrite(tens,     LOW); 
+      digitalWrite(sendPin,  LOW);
+      break;      
+    default: 
+      digitalWrite(hundreds, LOW); 
+      digitalWrite(tens, LOW); 
+      digitalWrite(sendPin, LOW);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  switching = 1;
+  switching = 1; statu = 0;
   pinMode(ledPin, OUTPUT);
   pinMode(sendPin, OUTPUT);
+  pinMode(tens, OUTPUT);
+  pinMode(hundreds, OUTPUT);
   #if !defined(__MIPSEL__)
     while (!Serial);
   #endif
@@ -42,7 +79,6 @@ void setup() {
 void loop() {
   Usb.Task();
   if (PS3.PS3Connected) {
-    digitalWrite(sendPin, HIGH);
     if (PS3.getButtonPress(PS) && PS3.getButtonPress(L2) && PS3.getButtonPress(R1)) {
       PS3.disconnect();
       //brake if breaks
@@ -64,19 +100,20 @@ void loop() {
           PS3.setLedOn(LED3);
     }
     if (PS3.getButtonClick(SQUARE)) {
-        
+        statuo = 2;
     }
     if (PS3.getButtonClick(TRIANGLE)) {
-        PS3.setRumbleOn(RumbleLow);
+        statuo = 1;
     }
     if (PS3.getButtonClick(CROSS)) {
-        
+        PS3.setRumbleOn(RumbleLow);
+        statuo = 3;
     }
     if (PS3.getButtonClick(CIRCLE)) {
         PS3.setRumbleOn(RumbleHigh);
+        statuo = 4;
     }
     if (PS3.getButtonClick(R1)) {
-        
     }
     if (PS3.getButtonClick(L1)) {
     }
@@ -87,6 +124,7 @@ void loop() {
     if (PS3.getButtonClick(L2)) {
     }    
     if (PS3.getButtonClick(R2)) {
-    }
-  } else digitalWrite(sendPin, LOW);//end PS3 Connect; brake if breaks
+    }    
+  }  else statuo = 0;  //end PS3 Connect
+  sendOut(statuo);
 }
